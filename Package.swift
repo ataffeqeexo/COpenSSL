b038dd6b0b5132b3da2fcfcd5f847f74997fc9a6
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2020-08-02 13:09:15 -0700

    update for Swift 5

--- Package.swift
+++ Package.swift
@@ -1,12 +1,19 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "COpenSSL",
-    pkgConfig: "openssl",
-    providers: [
-        .brew(["openssl"]),
-        .apt(["libssl-dev"])
+    products: [
+        .library(name: "COpenSSL", targets: ["COpenSSL"]),
+    ],
+    targets: [
+        .systemLibrary(name: "COpenSSL",
+            pkgConfig: "openssl",
+            providers: [
+                .brew(["openssl"]),
+                .apt(["libssl-dev"])
+            ]
+        )
     ]
 )
